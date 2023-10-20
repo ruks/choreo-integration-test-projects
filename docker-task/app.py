e63c7f98f23c8ba11b2da2e7b1c55dbe855408c3
ruks <rukshan@wso2.com>
2023-10-20 11:57:12 +0530

    Adding docker task

---
+++ docker-task/app.py
@@ -0,0 +1,6 @@
+print("Hello from Python inside Docker! 1")
+print("Hello from Python inside Docker! 2")
+print("Hello from Python inside Docker! 3")
+print("Hello from Python inside Docker! 4")
+print("Hello from Python inside Docker! 5")
+
